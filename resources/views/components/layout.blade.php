27a816cda32fd4698328c9309fc122027db02461
Kabbo_Hridoy <45655299+Kabbo-Positive@users.noreply.github.com>
2022-10-15 21:24:57 +0600

    Second update

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -30,7 +30,7 @@
     <a href="/"><img class="w-24" src="{{asset('images/logo.png')}}" alt="" class="logo" /></a>
     <ul class="flex space-x-6 mr-6 text-lg">
       @auth
-      <li>
+      <li class="fa-solid fa-user-plus">
         <span class="font-bold uppercase">
           Welcome {{auth()->user()->name}}
         </span>
@@ -68,7 +68,7 @@
   </main>
   <footer
     class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center">
-    <p class="ml-2">Copyright &copy; 2022, All Rights reserved Kabbo Hridoy</p>
+    <p class="ml-2">Copyright &copy; 2022, All Rights reserved Taj Tech Ltd</p>
 
     <a href="/listings/create" class="absolute top-1/3 right-10 bg-black text-white py-2 px-5">Post Job</a>
   </footer>
